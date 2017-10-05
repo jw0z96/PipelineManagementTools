e27f9692a2bb1bf3c84e814c1e3f0cc06ad64fee
Joe Withers <joewithers96@gmail.com>
2017-10-05 20:06:44 +0100

    added namespace py and ui files

---
+++ scripts/PipelineManagementTools/gui/nameSpaceDialog.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os.path
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtUiTools import *
+
+class NameSpaceDialog(QDialog):
+	def __init__(self, asset, parentWindow = None, *args, **kwargs):
+		super(NameSpaceDialog, self).__init__(parentWindow, *args, **kwargs)
+		self.selectedAsset = asset
+		self.initUI()
+
+	def initUI(self):
+		# load .ui file
+		currentDir = os.path.dirname(__file__)
+		file = QFile(currentDir+"/nameSpaceDialog.ui")
+		file.open(QFile.ReadOnly)
+		loader = QUiLoader()
+		self.ui = loader.load(file, parentWidget=self)
+		file.close()
+
+		# set regex for namespace
+		nameSpaceRegex = QRegExp("[A-Za-z0-9_]+")
+		nameSpaceValidator = QRegExpValidator(nameSpaceRegex, self.ui.nameSpaceLineEdit)
+		self.ui.nameSpaceLineEdit.setValidator(nameSpaceValidator)
+
+		# set callbacks & ui text
+		self.ui.nameSpaceLabel.setText("Enter unique namespace for: " + self.selectedAsset)
+		self.ui.buttonBox.rejected.connect(self.close)
+		self.ui.buttonBox.accepted.connect(self.accept)
