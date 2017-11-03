b0ce739fe9e39639923d84fc9830f489e309f964
Withers Joe <joewithers96@gmail.com>
2017-11-03 19:34:50 +0000

    testing rib export

--- scripts/PipelineManagementTools/flattenMayaScene.py
+++ scripts/PipelineManagementTools/flattenMayaScene.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python
 import maya.cmds as cmds
 import maya.mel as mel
+import os
 
-mel.eval('SelectAll') # ugly mel eval to select all top level?
-cmds.group(cmds.ls(sl=1), n="test_scene_export")
+# set a variable so all the textures load properly
+mel.eval('rman setvar MAYA_ASSET_DIR "$MAYA_ASSET_DIR"')
+
+# get the start frame and end frame from the render settings
+startFrame = cmds.getAttr('defaultRenderGlobals.startFrame')
+endFrame = cmds.getAttr('defaultRenderGlobals.endFrame')
+
+# directory to export into
+exportDirectory = "/home/i7463769/MAJOR_PROJECT/3_prod/anim/shot_01/archive_test/"
+
+# file name
+fileName = "test_archive"
+
+# set renderman export args
+rmanArgs = "rmanExportRIBCompression=0;rmanExportFullPaths=0;rmanExportGlobalLights=1;rmanExportLocalLights=1;rmanExportCoordinateSystems=0;rmanExportShaders=1;rmanExportAttributeBlock=0;rmanExportMultipleFrames=1;rmanExportStartFrame="+str(startFrame)+";rmanExportEndFrame="+str(endFrame)+";rmanExportByFrame=1"
+
+# export rib files
+cmds.file(os.path.join(exportDirectory,fileName+".rib"), f=True, op=rmanArgs, type="RIB_Archive", pr=True, ea=True)
+
+# what about textures
+
+# alembic export
+abcArgs = "-frameRange " + str(startFrame) + " " + str(endFrame) +" -ro -stripNamespaces -dataFormat ogawa -file " + os.path.join(exportDirectory,fileName+".abc")
+cmds.AbcExport(j = abcArgs)
