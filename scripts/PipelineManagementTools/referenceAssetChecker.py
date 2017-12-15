e70b59c427b5c3328b5cdad42cd3ec6017fb04c9
Joe Withers <joewithers96@gmail.com>
2017-12-15 19:40:08 +0000

    simplified referenceAssetChecker and removed a bug in which it would operate twice

--- scripts/PipelineManagementTools/referenceAssetChecker.py
+++ scripts/PipelineManagementTools/referenceAssetChecker.py
@@ -3,13 +3,18 @@
 import maya.cmds as cmds
 import os
 
-# assets directory specified by an environment variable
-assetDir = os.environ['MAYA_ASSET_DIR']
+def execute(operation):
+	# assets directory specified by an environment variable
+	assetDir = os.environ['MAYA_ASSET_DIR']
 
-# path to the assetChecker scene
-assetCheckerPath = os.path.join(assetDir, 'asset/assetChecker/assetChecker.ma')
+	# path to the assetChecker scene
+	assetCheckerPath = os.path.join(assetDir, 'asset/assetChecker/assetChecker.ma')
 
-#reference it in
-cmds.file(assetCheckerPath, r=True, namespace="TEMPORARY_LIGHTING")
-
-print "referenced in: " + assetCheckerPath
+	if operation:
+		#reference it in
+		cmds.file(assetCheckerPath, r=True, namespace="TEMPORARY_LIGHTING")
+		print "referenced in: " + assetCheckerPath
+	else:
+		# dereference it
+		cmds.file(assetCheckerPath, removeReference = True , f = True)
+		print "dereferenced: " + assetCheckerPath
