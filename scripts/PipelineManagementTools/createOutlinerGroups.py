fd16fa36026c6b80645c81af799973a816c33652
Joe Withers <joewithers96@gmail.com>
2017-12-20 17:26:46 +0000

    integrated default outliner groups script

---
+++ scripts/PipelineManagementTools/createOutlinerGroups.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""default_outlinerGroups.py: Script to create default Maya outliner group structure for layout/animation."""
+
+__author__	= "Yves Bedenikovic"
+__date__	= "07/12/2017"
+
+import maya.cmds as cmds
+
+def createGroups():
+	groupTitles = ["assets_GRP", "cams_GRP", "chars_GRP", "fx_GRP", "lights_GRP"]
+	for groupTitle in groupTitles:
+		cmds.group(em = True, name = groupTitle)
