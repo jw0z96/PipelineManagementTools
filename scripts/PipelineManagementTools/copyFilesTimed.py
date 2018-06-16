890c90c2111e29c40f520995b997523615091f21
Withers Joe <joewithers96@gmail.com>
2018-06-16 20:57:20 +0100

    copyfiles timed

--- scripts/PipelineManagementTools/copyFilesTimed.py
+++ scripts/PipelineManagementTools/copyFilesTimed.py
@@ -2,24 +2,29 @@ import os
 import time
 import shutil
 
-def listFiles(farmDir):
+def listFiles(farmDir, shot):
 	nonZeroFiles = []
 	files = os.listdir(farmDir)
-	print "files " + str(files)
 	for file in files:
 		filePath = os.path.join(farmDir, file)
-		if os.path.getsize(filePath) > 0:
+		if os.path.getsize(filePath) > 0 and file.startswith(shot):
 			nonZeroFiles.append(file)
 	return nonZeroFiles
 
+def moveFiles(shot, shotFolder):
+	farmDir = "/run/user/43049/gvfs/sftp:host=tete.bournemouth.ac.uk/home/i7463669/renders"
+	filesToMove = listFiles(farmDir, shot)
+	if len(filesToMove) > 0:
+		print shot + " filesToMove " + str(filesToMove) + " to " + shotFolder
+		for file in filesToMove:
+			shutil.move(os.path.join(farmDir, file), os.path.join(shotFolder, file))
+	else:
+		print "no files to move for " + shot
+
 def executeSomething():
-	farmDir = "/run/user/43049/gvfs/sftp:host=tete.bournemouth.ac.uk/home/i7463769/shot010_lighting/images"
-	dstDir = "/transfer/jay/shot010_lighting_frames"
-	filesToMove = listFiles(farmDir)
-	print "filesToMove " + str(filesToMove)
-	for file in filesToMove:
-		shutil.move(os.path.join(farmDir, file), os.path.join(dstDir, file))
-	#code here
+	moveFiles("shot060", "/transfer/.jay/frames/shot060/shot060_lighting_cached_v.0003")
+	moveFiles("shot110", "/transfer/.jay/frames/shot110/shot110_lighting_cached_v.0001")
+	moveFiles("shot120", "/transfer/.jay/frames/shot120/shot120_lighting_cached_v.0003")
 	time.sleep(600)
 
 while True:
